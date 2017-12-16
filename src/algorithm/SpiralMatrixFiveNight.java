2674ebbcccfcd9b64ed9fd96e45064b785c3c226
Janus <qihuaneng@163.com>
2017-12-16 22:49:46 +0800

    57 58

---
+++ src/algorithm/SpiralMatrixFiveNight.java
@@ -0,0 +1,19 @@
+/**
+ * For example,
+ Given n = 3,
+
+ You should return the following matrix:
+ [
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+ ]
+ */
+package algorithm;
+
+public class SpiralMatrixFiveNight {
+    public int[][] generateMatrix(int n) {
+return null;
+    }
+//    public int[][]
+}
