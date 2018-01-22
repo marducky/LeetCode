bc96208746690cc8d0d54e67135f25197dd23049
neng.qi <neng.qi@nuance.com>
2018-01-22 17:54:40 +0800

    Update -- Modify the file. MySQL -- insertInto 10000data.

--- src/majors/mysql/InsertIntoTestTable.java
+++ src/majors/mysql/InsertIntoTestTable.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class InsertIntoTestTable {
     public void insertIntoTestTables() {
+//        MySqlParam mySqlParam = new MySqlParam("jdbc:mysql://localhost:3306/refined_blood_hall?characterEncoding=utf8&useSSL=false", "root", "1314");
         MySqlParam mySqlParam = new MySqlParam("jdbc:mysql://localhost:3306/refined_blood_hall?characterEncoding=utf8&useSSL=false", "root", "1314");
         Connection connection = null;
         Statement statement = null;
@@ -23,13 +24,14 @@ public class InsertIntoTestTable {
             StringBuffer stringBufferTemp = null;
             for (int i = 0; i < 10000; i++) {
                 String id = Integer.toString(i);
-                String name = nameStrings[(random.nextInt(10) + 1) % 5];
+                String name = nameStrings[(random.nextInt(10) + 1) % 5] + Integer.valueOf(random.nextInt()).toString();
                 String sex = sexStrings[(random.nextInt(13)) % 4];
                 String slary = Integer.valueOf(random.nextInt()).toString();
                 String address = addressStrings[(random.nextInt(10) + 1) % 5];
                 String school = schoolStrings[(random.nextInt(10) + 1) % 5];
                 String isMar = isMarString[i % 2];
-                stringBuffer = new StringBuffer("INSERT INTO highqualitysql values(\"" + id + "\",\"" + name + "\",\"" + sex + "\",\"" + slary + "\",\"" + address + "\",\"" + school + "\",\"" + isMar + "\",\"unKnowm\");");
+//                stringBuffer = new StringBuffer("INSERT INTO highqualitysql values(\"" + id + "\",\"" + name + "\",\"" + sex + "\",\"" + slary + "\",\"" + address + "\",\"" + school + "\",\"" + isMar + "\",\"unKnowm\");");
+                stringBuffer = new StringBuffer("INSERT INTO highqualitysql_name_random values(\"" + id + "\",\"" + name + "\",\"" + sex + "\",\"" + slary + "\",\"" + address + "\",\"" + school + "\",\"" + isMar + "\",\"unKnowm\");");
                 System.out.println(stringBuffer.toString());
                 statement.execute(stringBuffer.toString());
 //                stringBuffer.reverse();
