2a0d28f9c6c909630329a7e7d773054141abace2
neng.qi <neng.qi@nuance.com>
2018-02-07 16:39:52 +0800

    Update -- Modify the file. Work -- Update for new tools.

---
+++ src/knowledge/javaCore/javaHighLevel/JavaFinalClassMethodParam.java
@@ -0,0 +1,63 @@
+package knowledge.javaCore.javaHighLevel;
+
+import java.util.Random;
+
+/**
+ * @project: knowledge.javaCore.javaHighLevel
+ * @author: Neng Qi
+ * @email: qihuaneng@163.com
+ * @date: 2018/02/07 16:26
+ **/
+class JavaFinalClassMethodParamPerson {
+    private String name;
+
+    JavaFinalClassMethodParamPerson(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class JavaFinalClassMethodParam {
+
+    private final String final_01 = "chenssy";    //编译期常量，必须要进行初始化，且不可更改
+    private final String final_02;                //构造器常量，在实例化一个对象时被初始化
+
+    private static Random random = new Random();
+    private final int final_03 = random.nextInt(50);    //使用随机数来进行初始化
+
+    //引用
+    public final JavaFinalClassMethodParamPerson final_04 = new JavaFinalClassMethodParamPerson("chen_ssy");    //final指向引用数据类型
+
+    JavaFinalClassMethodParam(String final_02) {
+        this.final_02 = final_02;
+    }
+
+    public String toString() {
+        return "final_01 = " + final_01 + "   final_02 = " + final_02 + "   final_03 = " + final_03 +
+                "   final_04 = " + final_04.getName();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("------------第一次创建对象------------");
+        JavaFinalClassMethodParam final1 = new JavaFinalClassMethodParam("cm");
+        System.out.println(final1);
+        System.out.println("------------第二次创建对象------------");
+        JavaFinalClassMethodParam final2 = new JavaFinalClassMethodParam("zj");
+        System.out.println(final2);
+        System.out.println("------------修改引用对象--------------");
+        final2.final_04.setName("chenssy");
+        System.out.println(final2);
+    }
+}
+/**
+ * 编译期常量
+ * 运行期初始化
+ * final数据、final方法、final类、final参数
+ */
---
+++ src/knowledge/javaCore/javaHighLevel/JavaForTheStaticAndCode.java
@@ -0,0 +1,54 @@
+package knowledge.javaCore.javaHighLevel;
+
+/**
+ * @project: knowledge.javaCore.javaHighLevel
+ * @author: Neng Qi
+ * @email: qihuaneng@163.com
+ * @date: 2018/02/07 16:24
+ **/
+public class JavaForTheStaticAndCode {
+    /**
+     * 静态代码块
+     */
+    static {
+        System.out.println("执行静态代码块...");
+    }
+
+    /**
+     * 构造代码块
+     */ {
+        System.out.println("执行构造代码块...");
+    }
+
+    /**
+     * 无参构造函数
+     */
+    public JavaForTheStaticAndCode() {
+        System.out.println("执行无参构造函数...");
+    }
+
+    /**
+     * 有参构造函数
+     *
+     * @param id
+     */
+    public JavaForTheStaticAndCode(String id) {
+        System.out.println("执行有参构造函数...");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("----------------------");
+        new JavaForTheStaticAndCode();
+        System.out.println("----------------------");
+        new JavaForTheStaticAndCode("1");
+    }
+}
+//静态代码块 > 构造代码块 > 构造函数。
+/**
+ * ----------------------
+ * 执行构造代码块...
+ * 执行无参构造函数...
+ * ----------------------
+ * 执行构造代码块...
+ * 执行有参构造函数...
+ */
