ddafef793ca4bc071c2cc22bebd01a116adf6913
neng.qi <neng.qi@nuance.com>
2018-03-28 17:38:47 +0800

    Update -- good luck, man.

--- src/algorithm/LintCode/LintCode.java
+++ src/algorithm/LintCode/LintCode.java
@@ -1,5 +1,8 @@
 package algorithm.LintCode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @project: algorithm
  * @author: Neng Qi
@@ -7,4 +10,9 @@ package algorithm.LintCode;
  * @date: 2018/02/22 14:24
  **/
 public class LintCode {
+    @Test
+    public void m3() {
+        M3_DigitCounts m3_digitCounts = new M3_DigitCounts();
+        System.out.println(m3_digitCounts.digitCounts(12, 1));
+    }
 }
