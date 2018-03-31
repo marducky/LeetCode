ae1abff48ea1b45f6634e4e0476c3d096f287633
Janus <qihuaneng@163.com>
2018-03-31 23:08:05 +0800

    good luck man.

---
+++ src/majors/openSource/DureOSApi/DuerOSCaller.java
@@ -0,0 +1,43 @@
+package majors.openSource.DureOSApi;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DuerOSCaller {
+    public String httpGet(String url, String charset)
+            throws HttpException, IOException {
+        String json = null;
+        HttpGet httpGet = new HttpGet();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 设置参数
+        try {
+            httpGet.setURI(new URI(url));
+        } catch (URISyntaxException e) {
+            throw new HttpException("请求url格式错误。" + e.getMessage());
+        }
+        // 发送请求
+        HttpResponse httpResponse = httpclient.execute(httpGet);
+        // 获取返回的数据
+        HttpEntity entity = httpResponse.getEntity();
+        byte[] body = EntityUtils.toByteArray(entity);
+        StatusLine sL = httpResponse.getStatusLine();
+        int statusCode = sL.getStatusCode();
+        if (statusCode == 200) {
+            json = new String(body, charset);
+            entity.consumeContent();
+        } else {
+            throw new HttpException("statusCode=" + statusCode);
+        }
+        return json;
+    }
+}
--- src/majors/openSource/DureOSApi/demo.java
+++ src/majors/openSource/DureOSApi/demo.java
@@ -1,4 +1,41 @@
 package majors.openSource.DureOSApi;
 
+import org.apache.http.HttpException;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import static java.util.UUID.randomUUID;
+
 public class demo {
+    //    @Autowired
+//    static DuerOSCaller duerOSCaller;
+    public static void main(String[] args) throws UnsupportedEncodingException, IOException, HttpException {
+        //main method
+        UrlBuilder urlBuilder = new UrlBuilder();
+        DuerosRequest duerosRequest = new DuerosRequest();
+        duerosRequest.setNApiUrl("http://sandbox.codriverapi.baidu.com/codriverapi/robokit");
+        duerosRequest.setAk("bn");
+        duerosRequest.setAv(3);
+        duerosRequest.setC("l");
+        duerosRequest.setCn("20011104");
+        duerosRequest.setCoordtype(2);
+//     duerosRequest.setUuid("add48e01-644c-41e9-82c7-585476ee28cc");
+        duerosRequest.setUuid(randomUUID().toString());
+        duerosRequest.setCrd("116.407396_39.9042");
+        duerosRequest.setWord("大汉天子主演是谁");
+//     duerosRequest.setWord("%E6%88%91%E7%9A%84%E5%89%8D%E5%8D%8A%E7%94%9F%E7%9A%84%E4%B8%BB%E6%BC%94%E6%98%AF%E8%B0%81");
+//     duerosRequest.setWord("%CE%D2%B5%C4%C7%B0%B0%EB%C9%FA%B5%C4%D6%F7%D1%DD%CA%C7%CB%AD");
+        urlBuilder.setDuerosRequest(duerosRequest);
+//        System.out.println(urlBuilder.buildNlpUrl());
+//     String str="我的前半生的主演是谁";
+//     try {
+//      System.out.println(URLEncoder.encode(str,"UTF-8"));
+//      System.out.println(URLDecoder.decode(str,"UTF-8"));
+//     }catch (UnsupportedEncodingException e){
+//      e.printStackTrace();
+//     }
+        DuerOSCaller duerOSCaller = new DuerOSCaller();
+        System.out.println(duerOSCaller.httpGet(urlBuilder.buildNlpUrl(), "GBK"));
+    }
 }
