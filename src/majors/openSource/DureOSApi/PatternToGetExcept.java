c3427b64e44fd5f37902e2826fc6338be8acfff3
Janus <qihuaneng@163.com>
2018-04-09 00:08:25 +0800

    good luck man.

--- src/majors/openSource/DureOSApi/PatternToGetExcept.java
+++ src/majors/openSource/DureOSApi/PatternToGetExcept.java
@@ -6,13 +6,16 @@ import java.util.regex.Pattern;
 public class PatternToGetExcept {
 
     public static void getExcept(String xText) {
-        String url = "[<div>]*?(.*)[</div>]*?<ul>[<li>(.*)</li>]*?(.*)</ul>(.*)";
+        String url = "[<div>]*?(.*)[</div>]*?.*<ul>(<li>.*</li>)*?</ul>.*";
         Pattern pattern = Pattern.compile(url);
         Matcher matcher = pattern.matcher(xText);
         int i = 0;
 //        System.out.println(matcher.find());
         while (matcher.find()) {
+            System.out.println(matcher.group(1));
             System.out.println(matcher.group(2));
+            System.out.println(matcher.groupCount());
+//            System.out.println(matcher.group(3));
         }
 
 
@@ -20,7 +23,8 @@ public class PatternToGetExcept {
 
     public static void main(String[] args) {
         //main method
-        String xText = "Do you mean<ul><li><a href=\"#\" data-vtz-link-type=\"Dialog\" data-vtz-reword-index=\"1\" data-vtz-interaction-index=\"2\" data-vtz-jump=\"4861704621517852538\">Can I use my routing number to receive international wire transfers?</a></li><li>ssssss</li></ul>";
+        String xText1 = "Do you mean<ul><li><a href=\"#\" data-vtz-link-type=\"Dialog\" data-vtz-reword-index=\"1\" data-vtz-interaction-index=\"2\" data-vtz-jump=\"4861704621517852538\">Can I use my routing number to receive international wire transfers?</a></li><li>ssssss</li></ul>";
+        String xText = "Do you mean<ul></ul>";
         getExcept(xText);
     }
 }
